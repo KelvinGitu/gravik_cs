62683fc2856ffb7af73198be843db1623269b85b
Kelvin Gitu <gitukelvin01@gmail.com>
2023-12-11 22:41:07 +0300

    fixed the paths issue

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,3 +7,6 @@ def main(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def contact(request): 
+    return render(request, 'contact.html')
